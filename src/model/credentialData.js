80e4f35badfe2e007f82c29379a5de54ea0d936b
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2021-05-14 22:31:58 +0530

    Server side authentication complete

--- src/model/credentialData.js
+++ src/model/credentialData.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('database_connection_link');
+mongoose.connect('mongodb+srv://hrishi:BhbQd6P2sVFYSxQ@hkm7.js3cl.mongodb.net/library?retryWrites=true&w=majority');
 
 const Schema = mongoose.Schema;
 
