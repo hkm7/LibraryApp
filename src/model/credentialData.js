09df4b46123583f4dfdc569e0ca1212dec5c54c0
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2021-05-18 17:11:13 +0530

    final touches

--- src/model/credentialData.js
+++ src/model/credentialData.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-
+mongoose.connect('mongodb+srv://hrishi:BhbQd6P2sVFYSxQ@hkm7.js3cl.mongodb.net/library?retryWrites=true&w=majority');
 //mongoose.connect('mongodb://localhost:27017/library');
 
 const Schema = mongoose.Schema;
