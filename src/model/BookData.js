622d05df05fc2c95ed6a6258eb456f937aad82a7
Hrishikesh Mohan <50671806+hkm7@users.noreply.github.com>
2021-05-18 15:43:51 +0530

    Added delete option

--- src/model/BookData.js
+++ src/model/BookData.js
@@ -2,7 +2,6 @@
 const mongoose = require('mongoose');
 
 //Database connection
-mongoose.connect('mongodb+srv://hrishi:BhbQd6P2sVFYSxQ@hkm7.js3cl.mongodb.net/library?retryWrites=true&w=majority');
 //mongoose.connect('mongodb://localhost:27017/library');
 
 //Schema Definition
